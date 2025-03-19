bb313dd1b06989e29724be229c4f731637e5cf0e
Michael Yang <michael.yang7@outlook.com>
2025-03-19 18:16:37 -0400

    Initial Commit

---
+++ cue/cueApp.swift
@@ -0,0 +1,17 @@
+//
+//  cueApp.swift
+//  cue
+//
+//  Created by Michael Yang on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct cueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cueTests/cueTests.swift
@@ -0,0 +1,17 @@
+//
+//  cueTests.swift
+//  cueTests
+//
+//  Created by Michael Yang on 3/19/25.
+//
+
+import Testing
+@testable import cue
+
+struct cueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
